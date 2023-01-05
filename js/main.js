618f0c6f93af50bb759f5a14536a9c4e5a12db05
Maslakov Oleksii <a.maslakov1991@gmail.com>
2023-01-05 12:58:30 +0200

    checkpoint
    checkpoint

--- js/main.js
+++ js/main.js
@@ -5,7 +5,20 @@ $(function(){
             controls: 0,
             showinfo: 0
         },
+    });
 
+    $('.characters__slider-img').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        asNavFor: '.characters__slider-text',
+        prevArrow: '<button class="slick-btn slick-prev"><img src="images/arrow-left.png" alt="prev"></button>',
+        nextArrow: '<button class="slick-btn slick-next"><img src="images/arrow-right.png" alt="next"></button>',
+    });
+    $('.characters__slider-text').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        asNavFor: '.characters__slider-img',
     });
 
 });
