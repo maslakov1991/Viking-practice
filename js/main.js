445ad8e4bf6a9d152394052e587589d1468f6266
Maslakov Oleksii <a.maslakov1991@gmail.com>
2023-01-06 12:50:04 +0200

    Checkpoint
    Checkpoint

--- js/main.js
+++ js/main.js
@@ -13,6 +13,15 @@ $(function(){
         asNavFor: '.characters__slider-text',
         prevArrow: '<button class="slick-btn slick-prev"><img src="images/arrow-left.png" alt="prev"></button>',
         nextArrow: '<button class="slick-btn slick-next"><img src="images/arrow-right.png" alt="next"></button>',
+        responsive: [
+            {
+                breakpoint: 769,
+                settings: {
+                    arrows: false,
+                    // dots: true
+                }
+            },
+        ]
     });
     $('.characters__slider-text').slick({
         slidesToShow: 1,
